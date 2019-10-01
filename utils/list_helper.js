a00ddb10fd8e88e9e9987f482cd143c2749cf8dc
Janne Lahtinen <janne.lahtinen@grafinote.fi>
2019-10-01 15:24:19 +0300

    apufunktioita ja yksikkötestejä, step 3 done

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,12 +3,22 @@ const dummy = (blogs) => {
   return 1
 }
 
+// Test returns the sum of all likes
 const totalLikes = (blogs) => {
   let likes = blogs.reduce((sum, blog) => sum + blog.likes, 0)
   return likes
 }
 
+// Test returns most liked blog
+const favoriteBlog = (blogs) => {
+  const likes = blogs.map(blog => blog.likes)
+  const mostLikes = Math.max(...likes)
+  const favorite = blogs.find( ({ likes }) =>  likes === mostLikes)
+  return favorite
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
