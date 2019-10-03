1639b1f3dc67f4ff8ac0e6faf5e0ed0d52d3bec3
Janne Lahtinen <janne.lahtinen@grafinote.fi>
2019-10-03 08:40:44 +0300

    apufunktioita ja yksikkötestejä, step 5 done

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,7 @@
 var _ = require('lodash')
 
+// eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => {
-  console.log(blogs)
   return 1
 }
 
@@ -22,18 +22,43 @@ const favoriteBlog = (blogs) => {
 // Test returns most prolific author and number of blogs
 const mostBlogs = (blogs) => {
   const blogSums =
-    _.countBy(blogs, 'author')
-  const maxValue =
-    _.max(_.values(blogSums))
-  const maxAuthor =
-    _.max(_.keys(blogSums))
-  const arr = { 'author': maxAuthor, 'blogs': maxValue }
-  return arr
+    _(blogs)
+      .countBy('author')
+      .map((objs, key) => ({
+        'author': key,
+        'blogs': _(objs).value()
+      }))
+      .value()
+  const maxBlogs = _.maxBy(blogSums, 'author')
+  //   _.countBy(blogs, 'author')
+  // const maxValue =
+  //   _.max(_.values(blogSums))
+  // const maxAuthor =
+  //   _.max(_.keys(blogSums))
+  // const arr = { 'author': maxAuthor, 'blogs': maxValue }
+  // console.log(blogSums)
+  return maxBlogs
+}
+
+// Test returns author with most likes
+const mostLikes = (blogs) => {
+  const auth =
+    _(blogs)
+      .groupBy('author')
+      .map((objs, key) => ({
+        'author': key,
+        'likes': _.sumBy(objs, 'likes')
+      }))
+      .value()
+  const maxLikes = _.maxBy(auth, 'likes')
+  // console.log('Tulos', maxLikes)
+  return maxLikes
 }
 
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
-  mostBlogs
+  mostBlogs,
+  mostLikes
 }
