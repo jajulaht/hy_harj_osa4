ac1f8da863400478bbb7e5258cfe326026e701bd
Janne Lahtinen <54285341+jajulaht@users.noreply.github.com>
2019-11-19 15:20:07 +0200

    build static

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ mongoose.connect(config.MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser
 app.use(cors())
 app.use(bodyParser.json())
 app.use(middleware.tokenExtractor)
+app.use(express.static('build'))
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
